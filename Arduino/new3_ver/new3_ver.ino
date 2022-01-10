e04d4afc5dc9e024dd6e1ab9fc51d84314f9d334
yagarwal123 <yashorajagarwal1999@gmail.com>
2022-01-10 14:00:40 +0000

    RFID

---
+++ Arduino/new3_ver/new3_ver.ino
@@ -0,0 +1,125 @@
+
+#include <Servo.h>
+#include "run_test.h"
+#include "dop.h"
+#include "read_id.h"
+#include "check_id_exist.h"
+#include "HX711.h"
+#include "load_cell.h"
+#include "read_id_sf.h" // include sparkfun decoder temporarily for testing
+#include "clear_serial_buffer.h"
+#define LOADCELL_DOUT_PIN  4
+#define LOADCELL_SCK_PIN  5
+#define calibration_factor 430.5 //This value is obtained using the SparkFun_HX711_Calibration sketch
+
+
+// define objects for door
+Servo door_one;  // create servo object to control a servo
+Servo door_two; // twelve servo objects can be created on most boards
+
+// define constants for RFID
+const int noMouse = 2;
+String KNOWNTAGS[noMouse] = {"0007A0F7C4", "0000000000"};
+String TAGNAMES[noMouse] = {"Stuart", "Little"};
+String ID, ID_sf;
+
+// objects and constants for weighing
+HX711 scale;
+float weight;
+
+// Variables for the lick and reward system
+int rewardPin = 32;
+int lickPin = A13;
+int THRESHOLD = 1000;
+
+unsigned long timeDoor1;
+unsigned long timeDoor2;
+String door1ID = "";
+String door2ID = "";
+boolean mouseAtDoorOne = false;
+boolean mouseAtDoorTwo = false;
+
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  Serial2.begin(9600);
+  door_one.attach(2);
+  door_open(door_one);
+  door_two.attach(23);
+  door_close(door_two);
+  /*
+    scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+    scale.set_scale(calibration_factor); //This value is obtained by using the SparkFun_HX711_Calibration sketch
+    scale.tare(); //Assuming there is no weight on the scale at start up, reset the scale to 0
+  */
+  //Setting up the pins for the reward system
+  pinMode(rewardPin, OUTPUT);
+  digitalWrite(rewardPin, LOW);
+}
+boolean door1Check(){
+  ID = read_id(Serial1);
+  if (ID.length() != 10) {
+    mouseAtDoorOne = false;
+  }
+  else{
+    unsigned long recordTime = millis();
+    Serial.println(ID);
+    String mouseName = check_id_exist(ID, KNOWNTAGS, TAGNAMES, noMouse);
+    Serial.println(mouseName);
+    if (mouseName != "Mouse does not exist") {
+      timeDoor1 = recordTime;
+      door1ID = ID;
+      mouseAtDoorOne = true; // if mouse exists in database, open the door
+      Serial.println("sdhsduhd");
+    }
+    else {mouseAtDoorOne = true;};
+  }
+  return mouseAtDoorOne;
+}
+
+boolean door2Check(){
+    // checks second antenna and see if tag exists
+  ID_sf = read_id_sf(Serial2);
+  if(ID_sf.length() == 12){
+    Serial.println(ID_sf); // if tag is recognised
+    mouseAtDoorTwo = true;
+  }
+  else {mouseAtDoorTwo = false;};
+  return mouseAtDoorTwo; 
+}
+  
+void loop()
+{
+  mouseAtDoorOne = door1Check();
+  // constantly checks until a known mouse appears
+  // can make this an interrupt or something
+  //Serial.println("aa");
+
+  //Serial.println(mouseAtDoorOne);
+  // clear buffer has no use here
+  //clear_serial_buffer(Serial1);
+ 
+    // checks second antenna and see if tag exists
+  //mouseAtDoorTwo = door2Check();
+
+  // TODO: if needed: wait for buffer to fill up if mouse 2 present
+  // then read buffer to see if mouse 2 present
+
+  
+
+  // take the weight
+//  weight = load_cell(scale);
+//  Serial.print(" weights: ");
+//  Serial.print(weight);
+//  Serial.println("g");
+//
+//  //delay(5000);
+//  Serial.println("closing door 2");
+//  door_close(door_two);
+//
+//  int liquidAmount = 500; // command from raspi
+//  run_test(lickPin, THRESHOLD, rewardPin, liquidAmount);
+  
+}
---
+++ Arduino/new3_ver/read_id.cpp
@@ -0,0 +1,50 @@
+#include "read_id.h"
+
+boolean check_tag_valid(char* newTag){
+
+  if ((newTag[10] == '\r')){
+    return true;
+  }
+  else {return false;};
+}
+
+String read_id(HardwareSerial &refSer){
+  /*
+    read until the first 13, then read the next 11 chars
+    tag is valid when the 11th char is 13 (\r)
+    return tag if tag is valid, else retun empty
+  */
+  String finalTag = "";
+  if (refSer.available()) { 
+    char newTag[11];
+    Serial.println("Serial available");
+    int i = 0;
+    int read_byte;
+    read_byte = refSer.read();
+    //Serial.println(read_byte);
+    while (read_byte != 13){
+      while (!refSer.available()){}; // wait until serial buffer is not empty
+      read_byte = refSer.read();//Serial.println(read_byte);
+    };//reach the first end of seq
+    while (i < 11) {
+      while (!refSer.available()){}; // better performance
+      read_byte = refSer.read();
+      //Serial.println(read_byte);
+      newTag[i] = read_byte;
+      i++;
+    }
+    Serial.println(newTag);
+    if (check_tag_valid(newTag)){
+      finalTag = newTag;
+      finalTag.trim(); // get rid of \r
+      finalTag = finalTag.substring(0, 11); // take the first 10 chars
+      return finalTag;
+      }
+    else {return "";};
+  }
+  else {return "";};
+}
+
+
+  
+ 
