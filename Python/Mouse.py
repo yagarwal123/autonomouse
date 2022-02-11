f8665754f9a98d7fc88d891f7f0b7dc21d7f323e
yagarwal123 <yashorajagarwak1999@gmail.com>
2022-02-11 01:52:00 +0000

    Adding module for matplotlib on gui

--- Python/Mouse.py
+++ Python/Mouse.py
@@ -3,8 +3,9 @@ class Mouse:
     def __init__(self,id,name,init_weight):
         self.id = id
         self.name = name
-        self.weight = [init_weight]
-        self.weight_times = [0]
+        self.init_weight = init_weight
+        self.weights = []
+        self.weight_times = []
         self.doortimes = []
         self.tests = []
 
@@ -15,7 +16,7 @@ class Mouse:
         return self.id
 
     def add_weight(self,w):
-        self.weight.append(w)
+        self.weights.append(w)
 
     def add_weighing_times(self,t):
         self.weight_times.append(t)
---
+++ Python/gui/mplwidget.py
@@ -0,0 +1,25 @@
+# Imports
+from PyQt6 import QtWidgets
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as Canvas
+import matplotlib
+
+matplotlib.use('QTAgg')
+
+# Matplotlib canvas class to create figure
+class MplCanvas(Canvas):
+    def __init__(self):
+        self.fig = Figure()
+        self.ax = self.fig.add_subplot(111)
+        Canvas.__init__(self, self.fig)
+        Canvas.setSizePolicy(self, QtWidgets.QSizePolicy.Policy.Expanding, QtWidgets.QSizePolicy.Policy.Expanding)
+        Canvas.updateGeometry(self)
+
+# Matplotlib widget
+class MplWidget(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)   # Inherit from QWidget
+        self.canvas = MplCanvas()                  # Create canvas object
+        self.vbl = QtWidgets.QVBoxLayout()         # Set box for plotting
+        self.vbl.addWidget(self.canvas)
+        self.setLayout(self.vbl)
