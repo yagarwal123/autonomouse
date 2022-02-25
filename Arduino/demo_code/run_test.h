c1a4ad846cb826ebb06ebf6a80e9eff175d472bc
yagarwal123 <yashorajagarwal1999@gmail.com>
2022-02-25 15:31:58 +0000

    everything in run_test - does not work

--- Arduino/demo_code/run_test.h
+++ Arduino/demo_code/run_test.h
@@ -1,8 +1,9 @@
 #ifndef run_test_h
 #define run_test_h
 #include "SdFat.h"
+#include "HX711.h"
+#include <Servo.h>
 
-
-void run_test(int lickPin, int THRESHOLD, int rewardPin, int liquidAmount, FsFile* pr, int WAITTIME);
+void run_test(Servo door_two, String ID_2, HX711 scale, int TTL_PIN, int lickPin, int rewardPin);
 
 #endif
