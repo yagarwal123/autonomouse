d655ce3904e7a4b301de386915905edc90c0726d
yagarwal123 <ya279@cam.ac.uk>
2022-03-26 18:19:39 +0000

    parse arg in config.py

--- Python/main.py
+++ Python/main.py
@@ -16,7 +16,6 @@ from gui.mainwin_actions import mainwinActions
 import sys
 import serial
 import multiprocessing
-import argparse
 
 from Mouse import Mouse
 import rasp_camera
@@ -35,24 +34,8 @@ import rasp_camera
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
-    
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--arduinoPath',metavar='',help="Arduino exe location")
-    parser.add_argument('--sketchPath',metavar='',help="Sketch ino location")
-    parser.add_argument('--TEENSY',action=argparse.BooleanOptionalAction)
-    parser.add_argument('--RASPBERRY',action=argparse.BooleanOptionalAction)
-    parser.add_argument('--PORT',metavar='',help="Specify port")
-    parser.add_argument('--OPEN_WINDOWS',action=argparse.BooleanOptionalAction)
-    # parser.add_argument('--application_path',metavar='',help="Specify mouse_info location and where to save files")
-    arg = parser.parse_args()
 
-    if arg.arduinoPath is not None: CONFIG.arduinoPath = arg.arduinoPath
-    if arg.sketchPath is not None: CONFIG.sketchPath = arg.sketchPath
-    if arg.TEENSY is not None: CONFIG.TEENSY = arg.TEENSY
-    if arg.RASPBERRY is not None: CONFIG.RASPBERRY = arg.RASPBERRY
-    if arg.PORT is not None: CONFIG.PORT = arg.PORT
-    if arg.OPEN_WINDOWS is not None: CONFIG.OPEN_WINDOWS = arg.OPEN_WINDOWS
-    # if arg.application_path is not None: CONFIG.application_path = arg.application_path
+    CONFIG.parse_arg()
 
     #m = subprocess.run(['C:/Program Files (x86)/Arduino/arduino.exe','--upload','C:\\Users\\lab\\AppData\\Local\\Temp\\arduino_build_680162/demo_code.ino.hex'])
     #m = subprocess.run(['"C:/PROGRA~2/Arduino/arduino.exe" --upload "C:/Users/lab/Desktop/autonomouse/Arduino/demo_code/demo_code.ino"/Arduino/arduino.exe" --upload "C:/Users/lab/Desktop/autonomouse/Arduino/demo_code/demo_code.ino"'],shell=True,encoding='UTF-8')
