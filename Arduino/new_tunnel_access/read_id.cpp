da0b2291b62c29f0afd8b4b49afa61271096ee85
Jieni <jieni0112@yahoo.com>
2022-04-25 16:39:43 +0100

    tunnel testing code

---
+++ Arduino/new_tunnel_access/read_id.cpp
@@ -0,0 +1,52 @@
+#include "read_id.h"
+
+boolean check_tag_valid(char* newTag){
+
+  if ((newTag[10] == '\r')){
+    return true;
+  }
+  else {return false;};
+}
+
+String read_id(HardwareSerial &refSer){
+  /*
+    read until the first 13, then read the next 11 chars
+    tag is valid when the 11th char is 13 (\r)
+    return tag if tag is valid, else retun empty
+  */
+  
+  if (refSer.available()) { 
+    char newTag[11];
+    Serial.println("LOGGER: Serial available");
+    int i = 0;
+    int read_byte;
+    read_byte = refSer.read();
+    //Serial.println(read_byte);
+    while (read_byte != 13){
+      //Serial.println("Reached here 1");
+      while (!refSer.available()){}; // wait until serial buffer is not empty
+      read_byte = refSer.read();//Serial.println(read_byte);
+    };//reach the first end of seq
+    
+    unsigned long strt_time = millis();
+    while ( (i < 11) && ( (millis() - strt_time) < 1000) ) { // why are we reading twice?
+      while (refSer.available()){ // better performance
+        read_byte = refSer.read();
+        //Serial.println(read_byte);
+        newTag[i] = read_byte;
+        i++;
+      }
+    }
+    if (check_tag_valid(newTag)){
+      String finalTag = newTag;
+      finalTag.trim();
+      return finalTag;
+      }
+    else {return "";};
+  }
+  else {return "";};
+}
+
+
+  
+ 
