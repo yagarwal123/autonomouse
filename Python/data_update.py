0ac4914a454b34440a18c24bb0602b589b7f8cfb
yagarwal123 <yashorajagarwal1999@gmail.com>
2022-06-30 18:40:00 +0100

    minor debug

--- Python/data_update.py
+++ Python/data_update.py
@@ -64,7 +64,7 @@ def dataUpdate(START_TIME,mutex,ser, inSer,all_mice,doors,live_licks,last_test,e
             last_test.add_trial(Trial(trial,t,stimuli))
         case 6:
             test = last_test
-            fileFolder = f' {test.id}'
+            fileFolder = test.id
             if not os.path.exists(fileFolder):
                 os.makedirs(fileFolder)
             filename = f'Test data - {test.id}.csv'
@@ -84,7 +84,7 @@ def dataUpdate(START_TIME,mutex,ser, inSer,all_mice,doors,live_licks,last_test,e
             
         case 7:
             test = last_test
-            fileFolder = f'{test.id}'
+            fileFolder = test.id
             filename = f'Raw lick data - {test.id}.csv'
             filePath = os.path.join(CONFIG.application_path,fileFolder,filename)
             mutex.unlock()
