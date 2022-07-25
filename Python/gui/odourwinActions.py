5f32c6dcf97e4cc729b2fe8ab9741c620f50ddaf
Jieni <jieni0112@yahoo.com>
2022-07-25 18:47:48 +0100

    debug

--- Python/gui/odourwinActions.py
+++ Python/gui/odourwinActions.py
@@ -42,14 +42,22 @@ class odourwinActions(QtWidgets.QWidget, Ui_odourWin):
     def generateOdour(self):
         # probability array for the number of odours
         nPrbArray = [0,0,0,0,0,0,0,0]
-        nPrbArray[0] = self.p1.text()
-        nPrbArray[1] = self.p2.text()
-        nPrbArray[2] = self.p3.text()
-        nPrbArray[3] = self.p4.text()
-        nPrbArray[4] = self.p5.text()
-        nPrbArray[5] = self.p6.text()
-        nPrbArray[6] = self.p7.text()
-        nPrbArray[7] = self.p8.text()
+        if self.p1.text() != '':
+            nPrbArray[0] = self.p1.text()
+        if self.p2.text() != '':
+            nPrbArray[1] = self.p2.text()
+        if self.p3.text() != '':
+            nPrbArray[2] = self.p3.text()
+        if self.p4.text() != '':
+            nPrbArray[3] = self.p4.text()
+        if self.p5.text() != '':
+            nPrbArray[4] = self.p5.text()
+        if self.p6.text() != '':
+            nPrbArray[5] = self.p6.text()
+        if self.p7.text() != '':
+            nPrbArray[6] = self.p7.text()
+        if self.p8.text() != '':
+            nPrbArray[7] = self.p8.text()
         nPrbArray = np.asarray(nPrbArray, dtype=float) # need to fill out all the boxes even if p=0
         if sum(nPrbArray)!=1:                   #Only number np.isnan(nPrbArray).any() 
             msg = QtWidgets.QMessageBox()
@@ -58,26 +66,38 @@ class odourwinActions(QtWidgets.QWidget, Ui_odourWin):
         
         # make the target array
         target = []
-        if self.checkBox.isChecked(): target.append(0)
-        if self.checkBox_2.isChecked(): target.append(1)
-        if self.checkBox_3.isChecked(): target.append(2)
-        if self.checkBox_4.isChecked(): target.append(3)
-        if self.checkBox_5.isChecked(): target.append(4)
-        if self.checkBox_6.isChecked(): target.append(5)
-        if self.checkBox_7.isChecked(): target.append(6)
-        if self.checkBox_8.isChecked(): target.append(7)
-        
-        # make target probability array
         targetProb = []
-        for i in target:
-            # temp = self.TP1.text()
-            #name = ['TP{}'.format(i)]
-            temp = self."TP"+str(i).text() # TODO: convert entered string into float
-            if temp.isnumeric() and temp<1:
-                targetProb.append(temp)
-            else:
-                msg = QtWidgets.QMessageBox()
-                msg.setText('Invalid target probability input') 
-                msg.exec()
+        if self.checkBox.isChecked() and self.TP1.text() != '': 
+            target.append(0)
+            targetProb.append(self.TP1.text())
+        if self.checkBox_2.isChecked() and self.TP2.text() != '': 
+            target.append(1)
+            targetProb.append(self.TP2.text())
+        if self.checkBox_3.isChecked() and self.TP3.text() != '': 
+            target.append(2)
+            targetProb.append(self.TP3.text())
+        if self.checkBox_4.isChecked() and self.TP4.text() != '': 
+            target.append(3)
+            targetProb.append(self.TP4.text())
+        if self.checkBox_5.isChecked() and self.TP5.text() != '': 
+            target.append(4)
+            targetProb.append(self.TP5.text())
+        if self.checkBox_6.isChecked() and self.TP6.text() != '': 
+            target.append(5)
+            targetProb.append(self.TP6.text())
+        if self.checkBox_7.isChecked() and self.TP7.text() != '': 
+            target.append(6)
+            targetProb.append(self.TP7.text())
+        if self.checkBox_8.isChecked() and self.TP7.text() != '': 
+            target.append(7)
+            targetProb.append(self.TP8.text())
+    
+        target = np.asarray(target, dtype=float) 
+        targetProb = np.asarray(targetProb, dtype=float) 
+        if sum(targetProb)!=1:                  
+            msg = QtWidgets.QMessageBox()
+            msg.setText('Invalid target input') 
+            msg.exec()
+        
         # call odour_gen function
         
